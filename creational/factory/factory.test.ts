9de76307451a14c88a3c5e9c6a8f9e497ff2c0ce
ante <ante.kotarac@gmail.com>
2023-06-13 23:19:17 +0200

    more factory tests

--- creational/factory/factory.test.ts
+++ creational/factory/factory.test.ts
@@ -6,9 +6,13 @@ describe('When called with parameter 1', () => {
 
 describe('When called with parameter 2', () => {
     test('should return instance of Product2 class ', () => expect(Factory.create(2)).toBeInstanceOf(Product2));
-    test('should return instance of Product2 class ', () => expect(Factory.create(2)).toHaveProperty('name', 'Product 2')); 
+    test('should return object that contains property name with value Product 2', () => expect(Factory.create(2)).toHaveProperty('name', 'Product 2')); 
 });
 
 describe('When called with non handled parameter', () => {
     test('should return instance of ProductGeneric class ', () => expect(Factory.create(22)).toBeInstanceOf(ProductGeneric));
+});
+
+describe('When called with non handled parameter', () => {
+    test('should return object that contains property name with value Generic Product', () => expect(Factory.create(22)).toHaveProperty('name', 'Generic Product')); 
 });
